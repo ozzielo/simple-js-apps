01b5934ee4d150c92817a98cc621a05b1183d5f7
ozzielo <57558395+ozzielo@users.noreply.github.com>
2020-12-10 23:24:01 -0500

    Revert "Removed Space"
    This reverts commit fdd0db163b1a7b8a42265b3da503215f1dced4be.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,5 @@
 let pokemonList = [
-  { name: "Bulbasaur",
-   height: 0.7,
-    type:["grass","poison"]},
-
-  { name: "Butterfree",
-   height: 1.1,
-    type:["bug","flying"]},
-
-  { name: "Beedrill",
-   height: 1,
-    type:["bug","poison"]}
+  { name: "bulbasaur", height: 0.7, type:["grass","poison"]},
+  { name: "butterfree", height: 1.1, type:["bug","flying"]},
+  { name: "beedrill", height: 1, type:["bug","poison"]}
 ];
-
-//This adds the pokemonList loop
-
-for (let i = 0; i < pokemonList.length; i++){
-  if ( pokemonList[i].height > 1){
-     document.write( pokemonList[i].name + " - " + "(height: " + pokemonList[i].height + ") " + " - Wow That Is Huge!!" + "<br>" + "<br>" );
-   }else {document.write( pokemonList[i].name + " - " + "(height: " + pokemonList[i].height + ") " + "<br>" + "<br>" );}
-
-
-}
