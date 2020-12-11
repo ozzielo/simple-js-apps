60f560329693476515fe930ba5732aaa0da9bdc9
ozzielo <57558395+ozzielo@users.noreply.github.com>
2020-12-10 19:52:39 -0500

    Added Semicolon

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,4 @@ let pokemonList = [
   { name: "bulbasaur", height: 0.7, type:["grass","poison"]},
   { name: "butterfree", height: 1.1, type:["bug","flying"]},
   { name: "beedrill", height: 1, type:["bug","poison"]}
-]
+];
