564e41df3f11bc67c2ffe487150e3dc30dd89cb5
ozzielo <57558395+ozzielo@users.noreply.github.com>
2020-12-16 19:31:07 -0500

    Added API to Repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,6 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: "Bulbasaur",
-      height: 0.7,
-      type:["grass","poison"]},
-
-    { name: "Butterfree",
-      height: 1.1,
-      type:["bug","flying"]},
-
-    { name: "Beedrill",
-      height: 1,
-      type:["bug","poison"]}
-  ];
+  let pokemonList = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
   // function pokemonLoop(pokemon) {
   function addListItem(pokemon) {
@@ -22,17 +11,14 @@ let pokemonRepository = (function () {
     button.classList.add("button-class");
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
-    button.addEventListener("click", function(event){showDetails(pokemon.name);})
+    button.addEventListener("click", function(event){showDetails(pokemon);})
   }
 
-  function showDetails(pokemon){
-    console.log(pokemon);
+  function showDetails(item){
+    pokemonRepository.loadDetails(item).then(function (){
+      console.log(item);
+    });
   }
-    // if (pokemon.height > 1){
-    //   document.write( pokemon.name + " - " + "(height: " + pokemon.height + ") " + " - Wow That Is Huge!!" + "<br>" + "<br>" );
-    // }else {document.write( pokemon.name + " - " + "(height: " + pokemon.height + ") " + "<br>" + "<br>" );}
-
-  // pokemonList.forEach(pokemonLoop);
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -42,65 +28,57 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+    function loadDetails(item){
+      let url = item.detailsUrl;
+      return fetch(url).then(function (response) {
+        return response.json();
+      }).then(function (details){
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      }).catch(function (e) {
+        console.error(e);
+      });
+  }
+
+
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails,
+
   };
 
 
 })();
 
-pokemonRepository.add({ name: "Venusaur" });
-console.log(pokemonRepository.getAll());
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+// pokemonRepository.add({ name: "Venusaur" });
+// console.log(pokemonRepository.getAll());
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
-
-
-//This adds the pokemonList loop
-
-// for (let i = 0; i < pokemonList.length; i++){
-//   if ( pokemonList[i].height > 1){
-//      document.write( pokemonList[i].name + " - " + "(height: " + pokemonList[i].height + ") " + " - Wow That Is Huge!!" + "<br>" + "<br>" );
-//    }else {document.write( pokemonList[i].name + " - " + "(height: " + pokemonList[i].height + ") " + "<br>" + "<br>" );}
-//
-//
-// }
-// function pokemonLoop(pokemon) {
-//   if (pokemon.height > 1){
-//     document.write( pokemon.name + " - " + "(height: " + pokemon.height + ") " + " - Wow That Is Huge!!" + "<br>" + "<br>" );
-//   }else {document.write( pokemon.name + " - " + "(height: " + pokemon.height + ") " + "<br>" + "<br>" );}
-// }
-// pokemonList.forEach(pokemonLoop);
-//
-//
-//
-// let pokemonRepository = (function () {
-//   let pokemonList = [
-//     { name: "Bulbasaur",
-//       height: 0.7,
-//       type:["grass","poison"]},
-//
-//     { name: "Butterfree",
-//       height: 1.1,
-//       type:["bug","flying"]},
-//
-//     { name: "Beedrill",
-//       height: 1,
-//       type:["bug","poison"]}
-//   ];
-//
-//   return {
-//     add: function(pokemon) {
-//       pokemonList.push(pokemon);
-//     },
-//     getAll: function() {
-//       return pokemonList;
-//     }
-//   };
-// })();
-//
-// console.log(pokemonRepository.getAll()); // []
-// pokemonRepository.add({ name: 'Pikachu' });
-// console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+// pokemonRepository.getAll().forEach(function (pokemon) {
+//   pokemonRepository.addListItem(pokemon);
+// });
