3f9604e29da5826ef46d1fbf6e74fa41962e3c06
ozzielo <57558395+ozzielo@users.noreply.github.com>
2021-01-04 14:38:21 -0500

    Added Modals

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ let pokemonRepository = (function () {
 
   function showDetails(item){
     pokemonRepository.loadDetails(item).then(function (){
-      console.log(item);
+      showModal(item);
     });
   }
 
@@ -72,6 +72,97 @@ let pokemonRepository = (function () {
 
 })();
 
+function showModal(pokemon) {
+  let modalContainer = document.querySelector("#modal-container");
+  modalContainer.innerHTML = "";
+  let modal = document.createElement("div");
+  modal.classList.add("modal");
+  let closeButtonElement = document.createElement("button");
+  closeButtonElement.classList.add("modal-close");
+  closeButtonElement.innerText = "Close";
+  closeButtonElement.addEventListener('click', hideModal);
+
+  let titleElement = document.createElement("h1");
+  titleElement.innerText = pokemon.name;
+
+  let contentHeight = document.createElement("p");
+  contentHeight.innerText = "Height: " + pokemon.height;
+
+  let imageElement = document.createElement("img");
+  imageElement.classList.add("modal-image");
+  imageElement.src = pokemon.imageUrl;
+
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentHeight);
+  modalContainer.appendChild(modal);
+  modal.appendChild(imageElement);
+
+  window.addEventListener('keydown', (e) => {
+  let modalContainer = document.querySelector('#modal-container');
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();
+  }
+});
+
+modalContainer.addEventListener('click', (e) => {
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+
+  modalContainer.classList.add('is-visible');
+
+}
+// document.querySelector("#show-modal").addEventListener("click", () => {
+//   showModal('Modal title', 'This is the modal content!');
+// });
+let dialogPromiseReject; // This can be set later, by showDialog
+
+function hideModal() {
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.classList.remove('is-visible');
+
+  if (dialogPromiseReject) {
+    dialogPromiseReject();
+    dialogPromiseReject = null;
+  }
+}
+
+function showDialog (title, text) {
+  showModal(title, text);
+  let modalContainer = document.querySelector("#modal-container");
+
+  let modal = modalContainer.querySelector(".modal");
+
+  let confirmButton = document.createElement("button");
+  confirmButton.classList.add("modal-confirm");
+  confirmButton.innerText = "Confirm";
+
+  let cancelButton = document.createElement("button");
+  cancelButton.classList.add("modal-cancel");
+  cancelButton.innerText = "Cancel";
+
+  modal.appendChild(confirmButton);
+  modal.appendChild(cancelButton);
+
+  confirmButton.focus();
+
+  return new Promise((resolve, reject) => {
+    cancelButton.addEventListener("click", hideModal);
+    confirmButton.addEventListener("click", () => {
+      dialogPromiseReject = null;
+      hideModal();
+      resolve();
+    });
+    dialogPromiseReject = reject;
+  });
+}
+
+
+
 // pokemonRepository.add({ name: "Venusaur" });
 // console.log(pokemonRepository.getAll());
 pokemonRepository.loadList().then(function() {
@@ -79,6 +170,17 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal('title', 'text');
+});
 // pokemonRepository.getAll().forEach(function (pokemon) {
 //   pokemonRepository.addListItem(pokemon);
 // });
+document.querySelector("#show-dialog").addEventListener("click", () => {
+  showDialog("Confirm action", "Are you sure you want to do this?").then(function (){
+    alert("Confirmed!");
+  }, () => {
+    alert("Not Confirmed");
+  });
+});
